530a616884303ac5e88cc214f1bacb1b953cdcbe
Coshea23 <cnoshea92@gmail.com>
2024-11-01 19:09:11 -0700

    Update PlayerButton.java

--- src/PlayerButton.java
+++ src/PlayerButton.java
@@ -1,3 +1,24 @@
+/**
+ * PlayerButton.java
+ * 
+ * This class represents a button in the Word Guessing Game that displays a 
+ * specific letter. Each button allows the player to select a letter as their guess. 
+ * The button holds its assigned letter as an attribute and includes a method to 
+ * add an ActionListener for handling player input.
+ * 
+ * Inherits: JButton from javax.swing package.
+ * 
+ * Attributes:
+ * - letter: Stores the character displayed on the button, representing the player's guess.
+ * 
+ * Methods:
+ * - getLetter(): Returns the character associated with this button.
+ * - addWordListener(ActionListener listener): Adds an ActionListener to handle the button click event.
+ * 
+ * Author: Javier & Chris
+ * Date: 29 Oct 24
+ * 
+ */
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
