c56f19a5d3a8e8c0fcbcabd8673015bda7b8db8f
Coshea23 <cnoshea92@gmail.com>
2024-11-01 19:06:05 -0700

    Update GuessButtonListener.java

--- src/GuessButtonListener.java
+++ src/GuessButtonListener.java
@@ -1,3 +1,21 @@
+/**
+ * GuessButtonListener.java
+ * 
+ * This class is an ActionListener for the Guess button in the Word Puzzle Game. 
+ * It captures the player's input, validates it as a single character guess, 
+ * and then invokes the game's checkGuess method to process the guess.
+ * 
+ * Dependencies: Requires the WordPuzzleGame class with access to its input 
+ * field and checkGuess method.
+ * 
+ * Methods:
+ * - actionPerformed(ActionEvent e): Handles the action event triggered 
+ *   by the Guess button and processes the player's input.
+ * 
+ * Author: Javier & Christopher
+ * Date: 29Oct24
+ * 
+ */
 import java.awt.event.ActionEvent;
 
 import java.awt.event.ActionListener;
